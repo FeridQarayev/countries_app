975884c8594e8a28ca154b6f66a5c3540abae18a
FeridQarayev <tu7he6n9s@code.edu.az>
2022-12-25 18:06:28 +0400

    add card html

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "Card.css";
 
 function Card() {
   return (
-    <div>Card</div>
-  )
+    <div className="card">
+      <img
+        className="card__img"
+        src="https://flagcdn.com/pk.svg"
+        alt="Pakistan"
+      />
+      <div className="card__content">
+        <h2>Pakistan</h2>
+        <p className="card__text">
+          <span>Population</span>: 220892331
+        </p>
+        <p className="card__text">
+          <span>Region</span>: Asia
+        </p>
+        <p className="card__text">
+          <span>Capital</span>: Islamabad
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
